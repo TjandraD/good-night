ff214e76e457a14deac3a343c8fce9f1c01a6d30
Tjandra Darmo <tjandradarmo@protonmail.com>
2025-09-11 09:17:09 +0700

    feat(#5): Update users' name constraint

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_020117) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 100, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
